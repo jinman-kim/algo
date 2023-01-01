0923826f60264b97bb7901324c2a49b45fa2b41f
jinmankim <gameliker16@naver.com>
2023-01-01 23:32:59 +0900

    [프로그래머스/파이썬]-마법의 엘리베이터

---
+++ 마법의 엘리베이터.py
@@ -0,0 +1,8 @@
+def solution(storey):
+    if storey < 10 :
+        return min(storey, 11 - storey)
+    left = storey % 10
+    return min(left + solution(storey // 10), 10 - left + solution(storey // 10 + 1))
+
+
+print(solution(2999))
