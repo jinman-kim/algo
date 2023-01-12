c9dc16efc3cb08be346cd6ceba87da98dc06be94
kjm4027 <gameliker16@naver.com>
2023-01-12 16:52:57 +0900

    20230112

---
+++ slime.py
@@ -0,0 +1,36 @@
+def solution(graph):
+    import time
+    start = time.time()
+    for i in graph:
+        i.insert(0,0)
+        i.insert(len(i),0)
+    graph.insert(0, [0 for _ in range(len(graph)+2)])
+    graph.insert(len(graph),[0 for _ in range(len(graph)+1)])
+    while True:
+        for i in range(len(graph)-1):
+            for j in range(len(graph)-1):
+                if graph[i][j] == 1:
+                    if graph[i+1][j] == 3 or graph[i-1][j] == 3 or graph[i][j+1] == 3 or graph[i][j-1] == 3 or graph[i+1][j]==2:
+                        pass
+                    else:
+                        if graph[i+1][j] == 0:
+                            graph[i][j] = 0
+                            graph[i+1][j] = 1
+        if time.time() - start > 0.2:
+            break
+    answer = []
+    for i in range(1,len(graph)-1):
+        answer.append(graph[i][1:len(graph)-1])
+    for i in answer:
+        print(i)
+    return answer
+
+
+graph = [
+    [1,1,1,3,1],
+    [0,1,1,1,3],
+    [1,0,0,1,1],
+    [1,2,1,2,1],
+    [3,2,0,0,0]]
+# 아래는 2가 있으면 안 되고, 3은 상하좌우에 다 있어도 안되고
+print(solution(graph))
