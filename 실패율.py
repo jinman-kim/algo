513a6838aab24ae346278aaa418c6eb67e476ed1
kjm4027 <gameliker16@naver.com>
2023-01-16 15:46:23 +0900

    실패율

---
+++ 실패율.py
@@ -0,0 +1,18 @@
+def solution(N,stages):
+    answer = {}
+    stages.sort()
+    tmp = len(stages)
+    for i in range(N):
+        if tmp:
+            answer[i+1] = (stages.count(i+1)/tmp)
+            tmp -= stages.count(i+1)
+        else:
+            answer[i+1] = 0
+    result = sorted(answer.items(), key = lambda x:-x[1])
+    a = [i[0] for i in result]
+    return a
+
+#
+N = 7
+stages = [1,2,3,4,5]
+print(solution(N,stages))
