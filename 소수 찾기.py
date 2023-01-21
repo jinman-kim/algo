42289501dadfbe48776c36a4d05254760031ba37
kjm4027 <gameliker16@naver.com>
2023-01-21 15:19:12 +0900

    2023012

---
+++ 소수 찾기.py
@@ -0,0 +1,27 @@
+def solution(numbers):
+
+    def is_prime(n):
+        if n == 1:
+            return 0
+        else:
+            for i in range(2, n + 1):
+                if n % i == 0 and i != n:
+                    return 0
+                elif i == n:
+                    return n
+
+    from itertools import permutations
+    answer = 0
+    combi = []
+    for num in range(len(numbers),0,-1):
+        for i in permutations(list(numbers),num):
+            combi.append(int(''.join(i)))
+    combi = list(set(combi))
+    for i in combi:
+        if is_prime(i):
+            answer+=1
+    return answer
+
+
+numbers = '17'
+print(solution(numbers))
