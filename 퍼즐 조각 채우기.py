1bd0f624ecce6f008f24ddecb6c444d1d90b60cb
kjm4027 <gameliker16@naver.com>
2023-02-01 20:57:13 +0900

    20230201

---
+++ 퍼즐 조각 채우기.py
@@ -0,0 +1,10 @@
+def solution(game_board, table):
+    answer = -1
+    for i in table:
+        print(i)
+    return answer
+
+
+game_board = [[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]]
+table = [[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],[0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]]
+print(solution(game_board, table))
