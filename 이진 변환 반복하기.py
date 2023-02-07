2b4cbc5687e999487167d5148bb230360991177f
kjm4027 <gameliker16@naver.com>
2023-02-07 15:15:41 +0900

    20230207

--- 이진 변환 반복하기.py
+++ 이진 변환 반복하기.py
@@ -1,15 +1,13 @@
 def solution(s):
-    s = list(map(int,list(s)))
     zero,trans = 0,0
     while True:
-        if 0 in s:
-            s.remove(0)
-            zero += 1
-        if 0 not in s:
-            s = list(map(int,list(format(len(s),'b'))))
-            trans += 1
-        if len(s) == 1:
+        if s == '1':
             break
+        zero += s.count('0')
+        s = s.replace('0','')
+        s = bin(len(s))[2:]
+        trans += 1
+
     return [trans,zero]
 
 
